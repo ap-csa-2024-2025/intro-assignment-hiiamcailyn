7414223fd4ab4e6395d7ccca3b3639155efd9b02
hiiamcailyn <cailyntran@gmail.com>
2024-09-03 10:11:25 -0700

    I love food

---
+++ Printing.java
@@ -0,0 +1,17 @@
+public class Printing
+{
+    public static void main(String[] args)
+    {
+        // int, doubt, boolean, string
+        int myAge = 999; // int is for whole numbers; integers
+        double price = 4.99;
+
+        boolean isGood; // variable declaration
+        isGood = false; // varaible initialization
+
+        String name = "Cailyn Tran"; // set anme to "Cailyn Tran"
+
+    System.out.println("Hello my name is " + name);
+    System.out.print("I love to eat fruits " + " I am " +isGood);
+    }
+}
