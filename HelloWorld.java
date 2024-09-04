187f0159595b165cd4e981b7f290f50b87323061
hiiamcailyn <cailyntran@gmail.com>
2024-09-04 10:10:21 -0700

    1.1 hw

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld
+
+
+// Problem 1, 2, 3
+{
+    public static void main(String[] args)
+    {
+        // int, doubt, boolean, string
+        int myAge = 999; // int is for whole numbers; integers
+        double price = 4.99;
+
+        boolean isGood; // variable declaration
+        isGood = false; // varaible initialization
+
+        String name = "Cailyn Tran"; // set name to "Cailyn Tran"
+
+        String favAuthor = "Shakespeare";  // set favAuthor to shakespear
+
+    System.out.println("                                                                My favorite author is " + favAuthor);
+    System.out.println("                                                                Coding is fun!");
+    System.out.println("                                                                Coding is fun!");
+    System.out.println("                                                                Coding is fun!");
+    System.out.println("=========\r\n" + //
+                                                                                       "= 0   0 =   Java is Awesome!\r\n" + //
+                                                                                       "=   v   =\r\n" + //
+                                                                                       "=========");
+    
+    }
+}
--- Printing.java
+++ Printing.java
@@ -3,12 +3,7 @@ public class Printing
     public static void main(String[] args)
     {
         // int, doubt, boolean, string
-        int myAge = 999; // int is for whole numbers; integers
-        double price = 4.99;
-
-        boolean isGood; // variable declaration
-        isGood = false; // varaible initialization
-
+     
         String name = "Cailyn Tran"; // set anme to "Cailyn Tran"
 
     System.out.println("Hello my name is " + name);
